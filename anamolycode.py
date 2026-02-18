0da93a2c23e91b78f8596d82d2d5e39f4da71388
Bharath-1602 <149683892+Bharath-1602@users.noreply.github.com>
2026-02-18 16:40:23 +0530

    Add files via upload

--- anamolycode.py
+++ anamolycode.py
@@ -1,7 +1,22 @@
 from collections import Counter
 
 with open('anamoly.log' , 'r') as f:
-    for lines in f:
-        words = lines.strip()
-        print(words)
+    lines = f.read()
+words = lines.split()
+lineslist = lines.split('\n')
+
+wordCount = Counter(words)
+tot = sum(wordCount.values())
+rarewords = []
+for i in wordCount.keys():
+    t = wordCount[i]
+    if (t/tot)*100 < 1:
+        rarewords.append(i)
+
+count = 1
+for i in lineslist:
+    for j in rarewords:
+        if j in i:
+            print(count)
+    count += 1
     
